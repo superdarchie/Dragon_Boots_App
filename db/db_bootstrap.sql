7268331a5e2521aaf91214ae11d362e8a7be019d
Mark Fontenot <mfonten@lyle.smu.edu>
2022-10-23 07:04:53 -0400

    Simple Flask with Redis template working

---
+++ db/db_bootstrap.sql
@@ -0,0 +1,13 @@
+create database northwind;
+use northwind;
+
+CREATE TABLE test_table (
+  name VARCHAR(20),
+  color VARCHAR(10)
+);
+
+INSERT INTO test_table
+  (name, color)
+VALUES
+  ('dev', 'blue'),
+  ('pro', 'yellow');
