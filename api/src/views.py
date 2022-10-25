a19239552f8416ec169ae8f036cebe969bf0d422
Mark Fontenot <mfonten@lyle.smu.edu>
2022-10-25 07:16:57 -0400

    major template updates for Blueprint and dbmodel

---
+++ api/src/views.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, request, jsonify, make_response
+import json
+import pymysql.cursors
+
+from . import db
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+    cursor = db.get_db().cursor()
+    cursor.execute('select customerNumber, customerName from customers')
+    theData = cursor.fetchall()
+
+    return jsonify(theData)
+    # return ('<h1>Hello from your web app!!</h1>')
