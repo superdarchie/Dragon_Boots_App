c9f28532908bc70a066af35d750d1a0c35e84886
Mark Fontenot <mfonten@lyle.smu.edu>
2022-10-25 11:27:06 -0400

    documentation update

--- api/src/__init__.py
+++ api/src/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flaskext.mysql import MySQL
 
+# create a MySQL object that we will use in other parts of the API
 db = MySQL()
 
 def create_app():
     app = Flask(__name__)
     
+    # Set up application configuration
     app.config['SECRET_KEY'] = 'someCrazyS3cR3T!Key.!'
+
+    # these are for the DB object to be able to connect to MySQL. 
     app.config['MYSQL_DATABASE_USER'] = 'webapp'
     app.config['MYSQL_DATABASE_PASSWORD'] = open('/secrets/db_password.txt').readline()
     app.config['MYSQL_DATABASE_HOST'] = 'db'
@@ -14,8 +18,7 @@ def create_app():
     app.config['MYSQL_DATABASE_DB'] = 'classicmodels'
 
     db.init_app(app)
-
-
+    
     from .views import views
 
     app.register_blueprint(views, url_prefix='/')
