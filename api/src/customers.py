cdf484b0b72b836d81d3a28e57cce69f9495367a
Mark Fontenot <mfonten@lyle.smu.edu>
2022-10-26 14:28:05 -0400

    updates to customer

--- api/src/customers.py
+++ api/src/customers.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, request, jsonify, make_response
-import json
-#import pymysql.cursors
+
 
 from . import db
 
@@ -9,14 +8,17 @@ customers = Blueprint('customers', __name__)
 @customers.route('/customers', methods=['GET'])
 def get_customers():
     cursor = db.get_db().cursor()
-    cursor.execute('select customerNumber, customerName, creditLimit from customers')
+    cursor.execute('select customerNumber, customerName,\
+        creditLimit from customers')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
     for row in theData:
         json_data.append(dict(zip(row_headers, row)))
-
-    return jsonify(json_data)
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
 
 @customers.route('/customers/<userID>', methods=['GET'])
 def get_customer(userID):
