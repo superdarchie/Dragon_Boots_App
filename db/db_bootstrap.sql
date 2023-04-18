6491d18abe91706b0891cf09d6647fd0a43682e9
superdarchie <116764202+superdarchie@users.noreply.github.com>
2023-04-18 10:35:04 -0400

    File organization

--- db/db_bootstrap.sql
+++ db/db_bootstrap.sql
@@ -1,33 +1,209 @@
--- This file is to bootstrap a database for the CS3200 project. 
+DROP DATABASE IF EXISTS jb_inc_db;
 
--- Create a new database.  You can change the name later.  You'll
--- need this name in the FLASK API file(s),  the AppSmith 
--- data source creation.
-create database cool_db;
+CREATE DATABASE jb_inc_db;
 
--- Via the Docker Compose file, a special user called webapp will 
--- be created in MySQL. We are going to grant that user 
--- all privilages to the new database we just created. 
--- TODO: If you changed the name of the database above, you need 
--- to change it here too.
-grant all privileges on cool_db.* to 'webapp'@'%';
+grant all privileges on jb_inc_db.* to 'webapp'@'%';
 flush privileges;
 
--- Move into the database we just created.
--- TODO: If you changed the name of the database above, you need to
--- change it here too. 
-use cool_db;
+USE jb_inc_db;
 
--- Put your DDL 
-CREATE TABLE test_table (
-  name VARCHAR(20),
-  color VARCHAR(10)
+CREATE TABLE Common_Attributes (
+    id_number INTEGER PRIMARY KEY AUTO_INCREMENT,
+    crystal_ball_number varchar(15) UNIQUE,
+    age INTEGER,
+    pay decimal,
+    realm varchar(50) NOT NULL,
+    username varchar(20) NOT NULL,
+    start_date DATETIME DEFAULT current_timestamp
 );
 
--- Add sample data. 
-INSERT INTO test_table
-  (name, color)
-VALUES
-  ('dev', 'blue'),
-  ('pro', 'yellow'),
-  ('junior', 'red');
+CREATE TABLE Shop (
+    galactic_port_id INTEGER,
+    realm VARCHAR(50),
+    description VARCHAR(200) NOT NULL,
+    open_time TIME NOT NULL,
+    close_time TIME NOT NULL,
+    PRIMARY KEY (galactic_port_id, realm)
+);
+
+CREATE TABLE Shopkeeper (
+    s_id INTEGER AUTO_INCREMENT,
+    id_number INTEGER NOT NULL,
+    favor tinyint NOT NULL,  # 0 - 255
+    galactic_port_id INTEGER,
+    realm VARCHAR(50),
+    PRIMARY KEY (s_id),
+    FOREIGN KEY (id_number) REFERENCES Common_Attributes(id_number) ON DELETE RESTRICT,
+    FOREIGN KEY(galactic_port_id, realm) REFERENCES Shop(galactic_port_id, realm) ON UPDATE CASCADE
+);
+
+CREATE TABLE Shopkeeper_Keys (
+    s_id INTEGER,
+    s_key VARCHAR(50),
+    PRIMARY KEY (s_id, s_key),
+    FOREIGN KEY (s_id) REFERENCES Shopkeeper(s_id) ON UPDATE CASCADE
+);
+
+CREATE TABLE Dragons (
+    name VARCHAR(50) PRIMARY KEY,
+    age INTEGER,
+    hp tinyint,
+    size VARCHAR(25),
+    weakness VARCHAR(50),
+    damage_type VARCHAR(50),
+    location VARCHAR(50)
+);
+
+CREATE TABLE Quests (
+    quest_id INTEGER PRIMARY KEY,
+    s_id INTEGER,
+    dragon_name VARCHAR(100),
+    reward INTEGER NOT NULL,
+    difficulty VARCHAR(3) NOT NULL,
+    description VARCHAR(200) NOT NULL,
+    completion_date DATETIME NOT NULL,
+    FOREIGN KEY (s_id) REFERENCES Shopkeeper(s_id) ON UPDATE CASCADE,
+    FOREIGN KEY (dragon_name) REFERENCES Dragons(name) ON UPDATE CASCADE
+);
+
+CREATE TABLE Hunters (
+    h_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id_number int NOT NULL,
+    hp tinyint NOT NULL,
+    damage tinyint NOT NULL,
+    speed tinyint NOT NULL,
+    defense tinyint NOT NULL,
+    quest_id INTEGER,
+    quest_date DATETIME,
+    FOREIGN KEY (id_number) REFERENCES Common_Attributes(id_number) ON DELETE RESTRICT,
+    FOREIGN KEY (quest_id) REFERENCES Quests(quest_id) ON UPDATE CASCADE
+);
+
+
+CREATE TABLE Forge (
+    forge_id INTEGER PRIMARY KEY,
+    magical_fuel INTEGER NOT NULL,
+    realm varchar(50) NOT NULL
+);
+
+CREATE TABLE Cobblers (
+    c_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id_number INTEGER NOT NULL,
+    title varchar(50) NOT NULL,
+    iq tinyint NOT NULL,
+    task varchar(100) NOT NULL,
+    forge_id INTEGER,
+    FOREIGN KEY (id_number) REFERENCES Common_Attributes(id_number) ON DELETE RESTRICT,
+    FOREIGN KEY (forge_id) REFERENCES Forge(forge_id) ON UPDATE CASCADE
+);
+
+CREATE TABLE Tools (
+    t_id INTEGER PRIMARY KEY,
+    h_id INTEGER,
+    c_id INTEGER,
+    name varchar(100) NOT NULL,
+    type varchar(50) NOT NULL,
+    material varchar(50) NOT NULL,
+    FOREIGN KEY (h_id) REFERENCES Hunters(h_id) ON UPDATE CASCADE,
+    FOREIGN KEY (c_id) REFERENCES Cobblers(c_id) ON UPDATE CASCADE
+);
+
+CREATE TABLE Boots (
+    boot_id INTEGER,
+    forge_id INTEGER,
+    material varchar(50) NOT NULL,
+    name varchar(100) NOT NULL,
+    size varchar(1),    # way to cap FK
+    galactic_port_id INTEGER,
+    realm varchar(50),
+    enchantment_1 varchar(50),
+    enchantment_2 varchar(50),
+    enchantment_3 varchar(50),
+    PRIMARY KEY (boot_id, forge_id),
+    FOREIGN KEY (forge_id) REFERENCES Forge(forge_id) ON UPDATE CASCADE,
+    FOREIGN KEY (galactic_port_id, realm) REFERENCES Shop(galactic_port_id, realm) ON UPDATE CASCADE
+);
+
+CREATE TABLE Scales (
+    type varchar(50),
+    dragon_name varchar(100),
+    damage_bonus TINYINT,
+    durability TINYINT,
+    protection_type TINYINT,
+    color varchar(25) NOT NULL,
+    forge_id INTEGER,
+    galactic_port_id INTEGER,
+    realm varchar(50),
+    stock_quantity INTEGER,
+    forge_quantity INTEGER,
+    PRIMARY KEY (type, dragon_name),
+    FOREIGN KEY (dragon_name) REFERENCES Dragons(name),
+    FOREIGN KEY (forge_id) REFERENCES Forge(forge_id) ON UPDATE CASCADE,
+    FOREIGN KEY (galactic_port_id, realm) REFERENCES Shop(galactic_port_id, realm) ON UPDATE CASCADE
+);
+
+CREATE TABLE Potions (
+    potion_id INTEGER PRIMARY KEY,
+    ingredients TEXT NOT NULL,
+    time_to_make decimal NOT NULL,
+    damage_bonus TINYINT,
+    protection_type TINYINT,
+    duration decimal,
+    galactic_port_id INTEGER,
+    realm varchar(50),
+    stock_quantity INTEGER,
+    FOREIGN KEY (galactic_port_id, realm) REFERENCES Shop(galactic_port_id, realm) ON UPDATE CASCADE
+);
+
+CREATE TABLE Wizards (
+    id_numb INTEGER PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    crystal_ball_number VARCHAR(50) UNIQUE,
+    address VARCHAR(50),
+    realm VARCHAR(50),
+    gold_vault_number INTEGER UNIQUE
+);
+
+CREATE TABLE Orders (
+    order_number INTEGER,
+    wizard_id INTEGER,
+    address VARCHAR(50) NOT NULL,
+    realm VARCHAR(50) NOT NULL,
+    gold_vault_number INTEGER NOT NULL,
+    s_id INTEGER,
+    PRIMARY KEY(order_number, wizard_id),
+    FOREIGN KEY(wizard_id) REFERENCES Wizards(id_numb) ON UPDATE CASCADE,
+    FOREIGN KEY (s_id) REFERENCES Shopkeeper(s_id) ON UPDATE CASCADE
+);
+
+CREATE TABLE Potion_Orders (
+    order_number INTEGER,
+    potion_id INTEGER,
+    quantity INTEGER NOT NULL,
+    unit_price DECIMAL NOT NULL,
+    PRIMARY KEY (order_number, potion_id),
+    FOREIGN KEY (order_number) REFERENCES Orders(order_number),
+    FOREIGN KEY (potion_id) REFERENCES Potions(potion_id)
+);
+
+CREATE TABLE Boot_Orders (
+    order_number INTEGER,
+    boot_id INTEGER,
+    forge_id INTEGER,
+    quantity INTEGER NOT NULL,
+    unit_price DECIMAL NOT NULL,
+    PRIMARY KEY(order_number, boot_id, forge_id),
+    FOREIGN KEY(order_number) REFERENCES Orders(order_number),
+    FOREIGN KEY (boot_id, forge_id) REFERENCES Boots(boot_id, forge_id)
+);
+
+CREATE TABLE Scale_Orders (
+    order_number INTEGER,
+    type VARCHAR(50),
+    dragon_name VARCHAR(50),
+    quantity INTEGER NOT NULL,
+    unit_price DECIMAL NOT NULL,
+    PRIMARY KEY(order_number, type, dragon_name),
+    FOREIGN KEY(order_number) REFERENCES Orders(order_number),
+    FOREIGN KEY (type, dragon_name) REFERENCES Scales(type, dragon_name)
+);
--- secrets/README.md
+++ secrets/README.md
@@ -7,4 +7,4 @@ In this folder, you'll need to create two files:
 - `db_password.txt`
   - in this file, put a password that will be used for a non-root db user
 - `db_root_password.txt`
-  - in this file, put the password you want to use for the root user (superuser) of mysql. 
+  - in this file, put the password you want to use for the root user (superuser) of mysql.
